34a891ceff5ed1a1e6e6a9a99b16acb67e30b8fd
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-22 14:26:52 +0200

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "almost-white": "hsl(0, 0%, 98%)",
+        "medium-gray": "hsl(0, 0%, 41%)",
+        "almost-black": "hsl(0, 0%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
